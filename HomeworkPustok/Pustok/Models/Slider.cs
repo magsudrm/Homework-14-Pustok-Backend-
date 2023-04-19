8cbce9455935e6fb3ec22b42ab1e0b3148a593fd
unknown <magsudrm@code.edu.az>
2023-04-20 02:12:17 +0400

    Project ended

---
+++ HomeworkPustok/Pustok/Models/Slider.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pustok.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        [MaxLength(30)]
+        public string Title1 { get; set; }
+        [MaxLength(30)]
+        public string Title2 { get; set; }
+        [MaxLength(150)]
+        [Required]
+        public string Image { get; set; }
+        [MaxLength(60)]
+        [Required]
+        public string ButtonText { get; set; }
+        [MaxLength(220)]
+        public string ButtonUrl { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; }
+    }
+}
